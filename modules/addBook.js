8c33aba116cd4f10c57eaa6d5ffc3f0cf04a2d58
Emam96 <emamsh990@gmail.com>
2021-08-25 17:37:31 +0300

    delet and add sone

--- modules/addBook.js
+++ modules/addBook.js
@@ -1,14 +1,12 @@
 const axios = require("axios");
-module.exports = addBook; 
+module.exports = addBook;
 
 const bookModel = require("./model.js");
 
 async function addBook(req, res) {
-    let { email, title, description } = req.body;
-  
-   
-   
-   await bookModel.create({ title, description, email });
+  let { email, title, description } = req.body;
+
+  await bookModel.create({ title, description, email });
 
   bookModel.find({ email: email }, function (err, books) {
     if (err) {
@@ -18,56 +16,4 @@ async function addBook(req, res) {
       res.status(201).send(books);
     }
   });
-   
-
 }
-
-
-//   async function addCatHandler(req, res) {
-//     console.log(req.body);
-//     // { catName: 'mishmish', catBreed: 'persian', ownerName: 'razan' }
-//     // let owner = req.body.ownerName;
-//     // let name = req.body.catName;
-//     // let breed = req.body.catBreed;
-//     let { catName, catBreed, ownerName } = req.body;
-//     // const newCat = new kittenModel({
-//     //     ownerName: ownerName,
-//     //     catName: catName,
-//     //     catBreed: catBreed
-//     // })
-
-//     // await newCat.save();
-
-//     await kittenModel.create({ownerName,catName,catBreed})
-//     // await kittenModel.create(req.body)
-
-
-//     kittenModel.find({ ownerName }, function (err, ownerData) {
-//         if (err) {
-//             console.log('error in getting the data')
-//         } else {
-//             console.log(ownerData);
-//             res.send(ownerData)
-//         }
-//     })
-
-
-
-// }
-
-
-
-
-// bookModel.findOne({ email: email }, function (error, books) {
-    //   if (error) {
-    //     res.send("NO DATA :(");
-    //   } else {
-    //     books.push({
-    //       title,
-    //       description,
-    //     });
-  
-    //     books.save();
-    //     res.status(201).send(books);
-    //   }
-    // })
--- modules/deleteBook.js
+++ modules/deleteBook.js
@@ -1,43 +1,49 @@
 const axios = require("axios");
-module.exports = deleteBook; 
+module.exports = deleteBook;
 
 const bookModel = require("./model.js");
 
-
 function deleteBook(req, res) {
-    let index = Number(req.params.id);
-  
-    let email = req.query.email;
-  
-    // bookModel.findOne({ email: email }, (error, books) => {
-    //   if (error) {
-    //     res.send("NO DATA :(");
-    //   } else {
-    //     let books2 = books.filter((book, i2) => {
-    //       if (i2 !== index) {
-    //         return book;
-    //       }
-    //     });
-    //     // console.log(books2);
-    //     books = books2;
-    //     console.log(books[0]);
-    //         books.save();
-    //     res.send(books);
-    //   }
-    // });
-  
-    bookModel.remove({ _id: index }, (error, books) => {
-      if (error) {
-        console.log("error in deleteing the data");
-      } else {
-        bookModel.findOne({ email: email }, function (err, books) {
-          if (err) {
-            console.log("error in getting the data");
-          } else {
-            books.save();
-            res.send(books);
-          }
-        });
-      }
-    });
-  }
+  let email = req.query.email;
+  let bookID = req.params.id;
+
+  bookModel.remove({ _id: bookID }, (error, bookData) => {
+    if (error) {
+      res.status(403).send("error in deleting the data");
+    } else {
+      console.log("data deleted", bookData);
+      bookModel.find({ email: email }, function (err, books) {
+        if (err) {
+          console.log("error in getting the data");
+        } else {
+          // console.log(books);
+          res.send(books);
+        }
+      });
+    }
+  });
+}
+
+// function deleteCatHandler(req,res) {
+//   // console.log(req.query.catID)
+//   console.log('inside the deleteCatHandler')
+//   console.log(req.params)
+//   console.log(req.params.catId2);
+//   let ownerName= req.query.ownerName;
+
+//   let catDataID = req.params.catId2;
+//   kittenModel.remove({_id:catDataID},(error,catData)=>{
+//       if(error) {
+//           console.log('error in deleteing the data')
+//       } else {
+//           console.log('data deleted', catData)
+//           kittenModel.find({ ownerName }, function (err, ownerData) {
+//               if (err) {
+//                   console.log('error in getting the data')
+//               } else {
+//                   console.log(ownerData);
+//                   res.send(ownerData)
+//               }
+//           })
+//       }
+//   })
